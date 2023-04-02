003f62c209d770e5b0dd59e53e5e75d1d66701ef
Anastasiia <na.konsta@gmail.com>
2023-04-02 22:16:29 +0300

    init

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = '9162c39fa9bb2a0a25906a9dfaa5b6f7'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/styles/main.sass'
+
+createApp(App).mount('#app')
